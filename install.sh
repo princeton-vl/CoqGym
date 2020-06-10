0257d16aeb4c13e22bf3661dc7b008f4fffba722
Kaiyu Yang <kaiyuy@princeton.edu>
2020-06-10 15:08:39 -0400

    update the installation script

--- install.sh
+++ install.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
+set -e
 
 OPAM_SWITCH="4.07.1+flambda"
-COQ_DEPENDENCIES="coq"
 COQ_ROOT=$(pwd)/coq
+DEPENDENCIES="dune=2.6.0 cmdliner=1.0.4 ppx_sexp_conv=v0.12.0 ppx_deriving=4.4 sexplib=v0.12.0 ppx_import=1.6.2 camlp5=7.08 coq=8.10.2"
 
-SERAPI_DEPENDENCIES="dune cmdliner ppx_sexp_conv ppx_deriving sexplib ppx_import"
+echo "Installing Dependencies.."
+opam switch $OPAM_SWITCH && eval $(opam env)
+opam install --yes $DEPENDENCIES
+echo "Dependencies installed"
 
 echo "Installing Coq.."
-opam switch $OPAM_SWITCH && eval $(opam env)
-opam install --yes $COQ_DEPENDENCIES
 cd coq
 ./configure -local -flambda-opts '-O3 -unbox-closures' && make
 cd ..
 export COQBIN=$COQ_ROOT/bin/
 export PATH=$COQBIN:$PATH
+echo "Coq installed"
 
 echo "Installing SerAPI.."
-opam install --yes $SERAPI_DEPENDENCIES
 cd coq-serapi
 make SERAPI_COQ_HOME=$COQ_ROOT
 dune install
 cd ..
+echo "SerAPI installed"
 
 echo "Installing CoqHammer.."
 cd ASTactic/coqhammer
 make
 make install
 cd ../..
+echo "CoqHammer installed"
 
-echo "Coq, SerAPI and CoqHammer installed."
