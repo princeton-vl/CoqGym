89dc5d03420785037e04187272792416d768a680
Kaiyu Yang <kaiyuy@princeton.edu>
2021-01-24 09:21:04 -0500

    Update unzip_data.py

--- unzip_data.py
+++ unzip_data.py
@@ -1,31 +1,42 @@
 import os
-import platform
 import sys
+import platform
 from hashlib import md5
-import pdb
 
 
-def execute(cmd):
+def execute(cmd: str) -> None:
     print(cmd)
     assert os.system(cmd) == 0
 
 
-def check_md5(filename, gt_hashcode):
-    print("checking %s.." % filename)
+def check_md5(filename: str, gt_hashcode: str) -> None:
+    """
+    Check the MD5 of a file against the ground truth
+    """
+    print("Checking %s.." % filename)
     if not os.path.exists(filename):
         print(filename, "not exists")
         print("aborting..")
         sys.exit(-1)
-    hashcode = md5(open(filename, "rb").read()).hexdigest()
+    # The file could be large
+    # See: https://stackoverflow.com/questions/48122798/oserror-errno-22-invalid-argument-when-reading-a-huge-file
+    inp = open(filename, "rb")
+    hasher = md5()
+    while True:
+        block = inp.read(64 * (1 << 20))
+        if not block:
+            break
+        hasher.update(block)
+    hashcode = hasher.hexdigest()
+
     if hashcode != gt_hashcode:
         print(filename, "has the wrong MD5 hashcode")
-        print("expect %s but found %s" % (gt_hashcode, hashcode))
-        print("aborting..")
+        print("Expect %s, but found %s" % (gt_hashcode, hashcode))
+        print("Aborting..")
         sys.exit(-1)
-    return
 
 
-def unzip(filename):
+def unzip(filename: str) -> None:
     if os.path.exists(filename[:-7]):
         remove = input(
             filename[:-7] + " already exists. Do you want to remove it? (y/N)"
@@ -39,27 +50,27 @@ def unzip(filename):
     execute("tar -xvzf " + filename)
 
 
-check_md5("projs_split.json", "39eac2315532040f370ca4996862ef75")
-check_md5("data.tar.gz", "922937155a199605eb8067ccfbbdb81a")
-check_md5("sexp_cache.tar.gz", "2e8ff40a7dd0b6d0efc74480dd3dfc8d")
+if __name__ == "__main__":
+    check_md5("projs_split.json", "39eac2315532040f370ca4996862ef75")
+    check_md5("data.tar.gz", "922937155a199605eb8067ccfbbdb81a")
+    check_md5("sexp_cache.tar.gz", "2e8ff40a7dd0b6d0efc74480dd3dfc8d")
+    unzip("data.tar.gz")
+    unzip("sexp_cache.tar.gz")
+    os.mkdir("sexp_cache")
+    execute("mdb_load -f sexp_cache.lmdb sexp_cache")
+    os.remove("sexp_cache.lmdb")
 
-unzip("data.tar.gz")
-unzip("sexp_cache.tar.gz")
-os.mkdir("sexp_cache")
-execute("mdb_load -f sexp_cache.lmdb sexp_cache")
-os.remove("sexp_cache.lmdb")
-
-print("setting the absolute paths..")
-cwd = os.getcwd()
-if platform.system() == "Darwin":
-    cmd = (
-        "find ./data -type f -exec sed -i '' 's/TAPAS_ROOT_ABSOLUTE_PATH/%s/g' {} +"
-        % cwd.replace(os.path.sep, "\/")
-    )
-else:
-    cmd = (
-        "find ./data -type f -exec sed -i 's/TAPAS_ROOT_ABSOLUTE_PATH/%s/g' {} +"
-        % cwd.replace(os.path.sep, "\/")
-    )
-execute(cmd)
-print("The CoqGym dataset is ready!")
+    print("Setting the absolute paths..")
+    cwd = os.getcwd()
+    if platform.system() == "Darwin":
+        cmd = (
+            "find ./data -type f -exec sed -i '' 's/TAPAS_ROOT_ABSOLUTE_PATH/%s/g' {} +"
+            % cwd.replace(os.path.sep, "\/")
+        )
+    else:
+        cmd = (
+            "find ./data -type f -exec sed -i 's/TAPAS_ROOT_ABSOLUTE_PATH/%s/g' {} +"
+            % cwd.replace(os.path.sep, "\/")
+        )
+    execute(cmd)
+    print("The CoqGym dataset is ready!")
