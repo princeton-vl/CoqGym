cc193b426718b8e7b6b14b1d4ceda27532b72763
Kaiyu Yang <kaiyuy@princeton.edu>
2021-01-24 20:59:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,46 @@
 #!/bin/bash
+
 OPAM_SWITCH="4.07.1+flambda"
 COQ_ROOT=$(pwd)/coq
 
 echo "Installing Dependencies.."
 opam switch $OPAM_SWITCH && eval $(opam env)
 opam install --yes dune=1.10.0 cmdliner=1.0.4 ppx_sexp_conv=v0.12.0 ppx_deriving=4.3 sexplib=v0.12.0 ppx_import=1.6.2 camlp5=7.08 coq=8.9.1
-echo "Dependencies installed"
 
-echo "Installing Coq.."
-cd coq
-./configure -local -flambda-opts '-O3 -unbox-closures' && make
-cd ..
-export COQBIN=$COQ_ROOT/bin/
-export PATH=$COQBIN:$PATH
-echo "Coq installed"
+if [ $? -eq 0 ]; then
+    echo "Dependencies installed\n"
+    
+    echo "Installing Coq.."
+    cd coq && ./configure -local -flambda-opts '-O3 -unbox-closures' && make && cd ..
 
-echo "Installing SerAPI.."
-cd coq-serapi
-make SERAPI_COQ_HOME=$COQ_ROOT
-dune install
-cd ..
-echo "SerAPI installed"
+    if [ $? -eq 0 ]; then
+        echo "Coq installed\n"
+        export COQBIN=$COQ_ROOT/bin/
+        export PATH=$COQBIN:$PATH
 
-echo "Installing CoqHammer.."
-cd ASTactic/coqhammer
-make
-make install
-cd ../..
-echo "CoqHammer installed"
+        echo "Installing SerAPI.."
+        cd coq-serapi && make SERAPI_COQ_HOME=$COQ_ROOT && dune install && cd ..
+         
+        if [ $? -eq 0 ]; then
+            echo "SerAPI installed\n"
 
+            echo "Installing CoqHammer.."
+            cd ASTactic/coqhammer && make && make install && cd ../..
+
+            if [ $? -eq 0 ]; then
+                echo "CoqHammer installed\n"
+            else
+                echo "Failed to install CoqHammer!"
+            fi
+
+        else
+            echo "Failed to install SerAPI!"
+        fi
+
+    else
+        echo "Failed to install Coq!" 
+    fi
+
+else
+    echo "Failed to install the dependencies!" 
+fi
