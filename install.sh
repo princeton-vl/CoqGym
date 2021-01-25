77eb7e0b49a01ab10e385f89141c312ebcaa92b6
Kaiyu Yang <kaiyuy@princeton.edu>
2021-01-24 21:05:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ opam switch $OPAM_SWITCH && eval $(opam env)
 opam install --yes dune=1.10.0 cmdliner=1.0.4 ppx_sexp_conv=v0.12.0 ppx_deriving=4.3 sexplib=v0.12.0 ppx_import=1.6.2 camlp5=7.08 coq=8.9.1
 
 if [ $? -eq 0 ]; then
-    echo "Dependencies installed\n"
+    echo "Dependencies installed"
     
     echo "Installing Coq.."
     cd coq && ./configure -local -flambda-opts '-O3 -unbox-closures' && make && cd ..
 
     if [ $? -eq 0 ]; then
-        echo "Coq installed\n"
+        echo "Coq installed"
         export COQBIN=$COQ_ROOT/bin/
         export PATH=$COQBIN:$PATH
 
@@ -22,13 +22,13 @@ if [ $? -eq 0 ]; then
         cd coq-serapi && make SERAPI_COQ_HOME=$COQ_ROOT && dune install && cd ..
          
         if [ $? -eq 0 ]; then
-            echo "SerAPI installed\n"
+            echo "SerAPI installed"
 
             echo "Installing CoqHammer.."
             cd ASTactic/coqhammer && make && make install && cd ../..
 
             if [ $? -eq 0 ]; then
-                echo "CoqHammer installed\n"
+                echo "CoqHammer installed"
             else
                 echo "Failed to install CoqHammer!"
             fi
