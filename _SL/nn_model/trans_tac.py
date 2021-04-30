4112a343d75feed66fee998d33581d499d17643d
Magnus Midtbø Kristiansen <magnumkr@stud.ntnu.no>
2021-05-01 01:01:24 +0200

    Update trans_tac.py

--- _SL/nn_model/trans_tac.py
+++ _SL/nn_model/trans_tac.py
@@ -17,7 +17,7 @@ class TransTac(nn.Module):
                                  num_labels = len(self.tactics),
                                  num_hidden_layers=self.opts.num_hidden,
                                  num_attention_heads=self.opts.num_attention,
-                                 vocab_size = 30522)
+                                 vocab_size = 28996) #30522
 
         self.bert= BertForSequenceClassification.from_pretrained('bert-base-uncased', config=self.config)
         #self.bert = BertForSequenceClassification(config=self.config)
